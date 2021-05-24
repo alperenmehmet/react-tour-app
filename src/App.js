c406833187c62d293bbbe6741d17a1cd19d09493
Mehmet ALPEREN <me.alperen@gmail.com>
2021-05-25 00:14:40 +0300

    initial create

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import data from "./data";
+import Tours from "./Tours";
 
 function App() {
-  return <h2>tour app</h2>;
+  const [tours, setTours] = useState(data);
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
+
+  const fetchTours = async () => {
+    const response = await fetch(data);
+    const tours = await response.json();
+    setTours(tours);
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>NO TOURS LEFT</h2>
+          <button className="btn" onClick={() => fetchTours()}>
+            refresh
+          </button>
+        </div>
+      </main>
+    );
+  }
+
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
+  );
 }
 
 export default App;
---
+++ src/Tour.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+const Tour = ({ id, img, info, name, price, removeTour }) => {
+  const [readMore, setReadMore] = useState(false);
+  return (
+    <article className="single-tour">
+      <img src={img} alt={name} />
+      <footer>
+        <div className="tour-info">
+          <h4>{name}</h4>
+          <h4 className="tour-price">{price}</h4>
+        </div>
+        <p>
+          {readMore ? info : `${info.substring(0, 200)}...`}
+          <button className="btn" onClick={() => setReadMore(!readMore)}>
+            {readMore ? "show less" : "  read more"}
+          </button>
+        </p>
+        <button className="btn-delete" onClick={() => removeTour(id)}>
+          not interested
+        </button>
+      </footer>
+    </article>
+  );
+};
+
+export default Tour;
